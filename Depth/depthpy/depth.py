23bbb91dc0851ada397f9b04f5b773ca163475e4
parkchamchi <parkchamchi@gmail.com>
2022-11-06 00:17:47 +0900

    v0.4.1-alpha

--- Depth/depthpy/depth.py
+++ Depth/depthpy/depth.py
@@ -23,7 +23,7 @@ from midas.midas_net import MidasNet
 from midas.midas_net_custom import MidasNet_small
 from midas.transforms import Resize, NormalizeImage, PrepareForNet
 
-VERSION = "v0.4-alpha"
+VERSION = "v0.4.1-alpha"
 
 class Runner():
 	
