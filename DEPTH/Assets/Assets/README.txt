4a991c2e8fbcf64bb3857d1643bfdfc058905f5e
parkchamchi <parkchamchi@gmail.com>
2022-11-14 19:43:42 +0900

    v0.5.7-beta

--- DEPTH/Assets/Assets/README.txt
+++ DEPTH/Assets/Assets/README.txt
@@ -1,4 +1,4 @@
-DepthViewer v0.5.6-beta
+DepthViewer v0.5.7-beta
 
 Right mouse key: hides the UI.
 WASD: rotate the mesh.
--- DEPTH/Assets/Scripts/DepthFileUtils.cs
+++ DEPTH/Assets/Scripts/DepthFileUtils.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using UnityEngine;
 
 public static class DepthFileUtils {
-	public const string Version = "v0.5.6-beta";
+	public const string Version = "v0.5.7-beta";
 	
 	public const string DepthExt = ".depthviewer";
 
