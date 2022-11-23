119d5f15d236959817dc50fd5a501a7f7431c6db
parkchamchi <parkchamchi@gmail.com>
2022-11-23 16:09:33 +0900

    connecting to the server

--- DEPTH/Assets/Scripts/ServerConnectBehavior.cs
+++ DEPTH/Assets/Scripts/ServerConnectBehavior.cs
@@ -2,7 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using TMPro;
 
 public class ServerConnectBehavior : MonoBehaviour {
-	
+	public TMP_InputField AddrIF;
+	public TMP_InputField ModelTypeIF;
+	public TMP_Text ServerStatusText;
+	public TMP_Text ModelStatusText;
+
+	private string _url;
+
+	public void Connect() {
+		string addr = AddrIF.text;
+		string modelType = ModelTypeIF.text;
+
+		string url = $"{addr}/depthpy/models/{modelType}";
+		StartCoroutine(GetRequest(url));
+	}
+
+	private IEnumerator GetRequest(string url) {
+
+		using (UnityWebRequest req = UnityWebRequest.Get(url)) {
+			yield return req.SendWebRequest();
+
+			if (req.result == UnityWebRequest.Result.Success) {
+				ServerStatusText.text = "OK";
+				ModelStatusText.text = _url = url;
+			}
+			else {
+				ServerStatusText.text = "Failed to connect";
+			}
+		}
+	}
+
+	public void Disconnect() {
+		_url = null;
+		ServerStatusText.text = "Disconnected.";
+		ModelStatusText.text = "";
+	}
 }
