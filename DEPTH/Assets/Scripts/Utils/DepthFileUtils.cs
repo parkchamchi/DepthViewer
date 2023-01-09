1c9e059e892ecc1d55b98280480b23b14ba11774
parkchamchi <parkchamchi@gmail.com>
2023-01-09 20:00:53 +0900

    v0.7.6-beta

--- DEPTH/Assets/Scripts/Utils/DepthFileUtils.cs
+++ DEPTH/Assets/Scripts/Utils/DepthFileUtils.cs
@@ -18,7 +18,7 @@ public enum ModelTypes : int {
 }
 
 public static class DepthFileUtils {
-	public const string Version = "v0.7.5-beta";
+	public const string Version = "v0.7.6-beta";
 	
 	public const string DepthExt = ".depthviewer";
 
