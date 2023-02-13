e43250597b80411f2a9c1edbe52b1d1baab0a156
parkchamchi <parkchamchi@gmail.com>
2023-02-13 09:50:48 +0900

    disabling/enabling `_moveMeshByMouse`

--- DEPTH/Assets/Scripts/MeshParentBehavior.cs
+++ DEPTH/Assets/Scripts/MeshParentBehavior.cs
@@ -1,18 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
+
+using IngameDebugConsole;
 
 public class MeshParentBehavior : MonoBehaviour {
 	/*
 		This rotates by mouse, but will restore to (0, 0, 0) by time
 	*/
 
-	private const float _acc = 1f;
+	private bool _moveMeshByMouse = true;
+	private Vector3 _lastMousePos; //can't be null
 
-	private Vector3 _lastMousePos;
+	void Start() {
+		DebugLogConsole.AddCommandInstance("set_moveMeshByMouse", "Whether the mesh would follow the mouse", "SetMoveMeshByMouse", this);
+	}
 
 	void Update() {
+		if (!_moveMeshByMouse)
+			return;
+
 		//Get the diff
 		Vector3 currentMousePos = Input.mousePosition;
 		Vector3 diff = currentMousePos - _lastMousePos;
@@ -24,7 +33,11 @@ public class MeshParentBehavior : MonoBehaviour {
 
 	void FixedUpdate() {
 		//Restore
-	
 		transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.identity, .05f);
+	}
+
+	public void SetMoveMeshByMouse(bool value) {
+		Debug.Log($"Setting _moveMeshByMouse to: {value}");
+		_moveMeshByMouse = value;
 	}
 }
