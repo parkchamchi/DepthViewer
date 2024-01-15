9e704fb171195bc9c57cafd3ebe86e02741e15e3
parkchamchi <parkchamchi@gmail.com>
2024-01-15 21:01:20 +0900

    fixed `gen_dummy_dmap.py`

--- DEPTH/depthpy/utils/gen_dummy_dmap.py
+++ DEPTH/depthpy/utils/gen_dummy_dmap.py
@@ -3,20 +3,26 @@ import matplotlib.pyplot as plt
 import cv2
 
 size = 512
-steps = 5
+steps = 4
 
-arr = np.empty((size, size), dtype=np.float32)
+assert steps % 2 == 0
+
+arr = np.zeros((size, size), dtype=np.float32)
 
 half = size / 2
-for i in np.arange(1, 0, -1/steps):	
-	from_idx = int(half - i * half)
-	to_idx = int(half + i * half)
+k = half / (steps+1)
+for i in range(steps):
+	disp = k * i
+	from_idx = int(disp)
+	to_idx = int(-disp)
+	val = i * (1 / (steps-1))
 
-	print(from_idx, to_idx)
+	print(from_idx, to_idx, val)
 
-	arr[from_idx:to_idx, from_idx:to_idx] = 1-i
+	arr[from_idx:to_idx, from_idx:to_idx] = val
 	
-plt.imshow(arr)
+plt.imshow(arr, cmap="gray")
+plt.colorbar()
 plt.show()
 
 arr = arr * 255
