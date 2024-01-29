b77aec08becedefd25d27195aadc3c46c733e20c
parkchamchi <parkchamchi@gmail.com>
2024-01-29 22:14:24 +0900

    v0.9.1

--- DEPTH/Assets/Scripts/Utils/DepthFileUtils.cs
+++ DEPTH/Assets/Scripts/Utils/DepthFileUtils.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using UnityEngine;
 
 public static class DepthFileUtils {
-	public const string Version = "v0.9.1-beta.6";
+	public const string Version = "v0.9.1";
 	
 	public const string DepthExt = ".depthviewer";
 
